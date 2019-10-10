ce1678a589d7c06c967dd12e7759ec7ed1877adc
Maliah Lacy <mlacy0915@gmail.com>
2019-10-10 11:51:27 -0500

    json added axios var added to js

---
+++ liri.js
@@ -0,0 +1,29 @@
+var axios = require("axios");
+
+// console.log(process.argv)
+
+
+
+// var string1 = process.argv[2];
+// var num2 = parseInt(process.argv[3]);
+// var num3 = parseInt(process.argv[4]);
+// var num4 = parseInt(process.argv[5]);
+// var num5 = parseInt(process.argv[6])
+
+// console.log(string1);
+// console.log(num2);
+// console.log(num3);
+// console.log(num4);
+// console.log(num5);
+
+// if (string1 === "add") {
+//     console.log(num2+num3+num4+num5);
+// }
+
+// else if (string1 === "multiply") {
+//     console.log(num2*num3*num4*num5);
+// }
+
+// else if (string1 === "divide") {
+//     console.log(num2/num3/num4/num5);
+// }
