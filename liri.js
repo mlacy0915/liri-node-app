c9c4f14ad2378c7d370a308fd9a0657abed256f6
Maliah Lacy <mlacy0915@gmail.com>
2019-10-13 13:28:26 -0500

    sudo coded & spotify code added

--- liri.js
+++ liri.js
@@ -6,10 +6,49 @@ var axios = require("axios");
 var Spotify = require('node-spotify-api');
 var omdb = require('omdb');
 var moment = require('moment');
-var bandsintown = require('bandsintown')(codingbootcamp);
+var bandsintown = require('bandsintown')(APP_ID);
 var keys = require("./keys");
 var spotify = new Spotify(keys.spotify);
 
+var artistName = function(artist) {
+    return artist.name
+}
+// going to have to call function later
+
+var searchSpotify = function(songName) {
+    if (songName === undefined) {
+        songName = "Whats my age again";
+    }
+    spotify.search (
+        {
+            type: "track",
+            query: "songName",
+        },
+        function(err,data) {
+            if (err) {
+                console.log("Error: " + err);
+                return; 
+            }
+            var songs = data.tracks.items;
+
+            for (i = 0; i < songs.length; i++ ) {
+                console.log(i);
+                console.log("artist: " + songs[i].artists.map(artistName));
+                console.log("songname: " + songs[i].name);
+                console.log("album: " + songs[i].album.name);
+            }
+        }
+    )
+}
+
+//band in town fuction 
+// call to api .axios call. for loop of data. moment to fomat data
+
+
+//movie search function,
+//if state similar to spotify default song = movie, 
+//axios call to omdb, console.log, 
+// do what it says function, set up err, switch case,
 
 // console.log(process.argv)
 
