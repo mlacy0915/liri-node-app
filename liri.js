2165e7bd040f9f7dd309df50327851cc8819933f
Maliah Lacy <mlacy0915@gmail.com>
2019-10-15 20:34:31 -0500

    concert this and movie this fixed changed var pick

--- liri.js
+++ liri.js
@@ -5,24 +5,25 @@ var axios = require("axios");
 var Spotify = require('node-spotify-api');
 var omdb = require('omdb');
 var moment = require('moment');
-var bandsintown = require('bandsintown')(APP_ID);
+// var bandsintown = require('bandsintown')(APP_ID);
 var keys = require("./keys");
 var spotify = new Spotify(keys.spotify);
 var require = require("request");
 
-function UserImputs (userOption, inputParameter) {
-    switch (userOption) {
+
+var pick = function(caseData, functionData) {
+    switch (caseData) {
         case 'concert-this':
-            showConcertInfo(inputParameter);
+            showConcertInfo(functionData);
             break;
         case 'spotify-this-song':
-            showSongInfo(inputParameter);
+            showSongInfo(functionData);
             break;
         case 'movie-this':
-            showMovieInfo(inputParameter);
+            showMovieInfo(functionData);
             break;
         case 'do-what-it-says':
-            showSomeInfo();
+            showSomeInfo(functionData);
             break;
     }
 }
@@ -58,37 +59,60 @@ var searchSpotify = function(songName) {
     )
 }
 //band in town fuction 
+var concertThis = function(artist) {
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    axios.get (queryUrl).then(
+        function(response) {
+            var jsonData = response.data;
+            if (!jsonData.length) {
+                console.log("No info found" + functionData
+        )
+                return;
+            }
+            for (var i = 0; i < jsonData.length; i++) {
+                var index = jsonData[i];
+
+                console.log(show.venue.city + "," + (show.venue.region||show.venue.country) + "at" + show.venue.name + " " + moment(show.datetime).format("MM/DD/YYYY"));
+
+            } 
+        }
+    )
+
+    
+}
 
 // call to api .axios call. for loop of data. moment to fomat data
 
 
 //movie search function,
-var movieThis = funtion(inputParameter) {
-    if (inputParameter === undefined) {
-        inputParameter = "Mr. Nobody"
+var movieThis = function(movieName) {
+    if (movieName === undefined) {
+        movieName = "Mr. Nobody"
         console.log("If you haven't watched 'Mr. Nobody,' then you should: <http://www.imdb.com/title/tt0485947/>");
     }
-    var queryUrl = "http://www.omdbapi.com/?t=" + inputParameter + "&y=&plot=short&apikey=codingbootcamp";
-    request(queryUrl, funtion(err.response,body) {
-        if (error.response.statusCode === 200) {
-            console.log("Title of the movie: " + JSON.parse(body).Title);
-            console.log("Year the movie came out: " + JSON.parse(body).Year);
-            console.log("IMDB Rating of the movie: " + JSON.parse(body).Rating);
-            console.log("Country produced: " + JSON.parse(body).Country);
-            console.log("Language of the movie: " + JSON.parse(body).Language);
-            console.log("Plot of the movie: " + JSON.parse(body).Plot);
-            console.log("Actors in the movie: " + JSON.parse(body).Actors);
-
-            for(var i = 0; i < JSON.parse(body).Ratings.length; i++) {
-                if(JSON.parse(body).Ratings[i].Source === "Rotten Tomatoes") {
-                    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[i].value);
-                    if(JSON.parse(body).Ratings[i].Website !== undefined) {
-                        console.log("Rotten Tomatoes URL: " + JSON.parse(body).Ratings[i].Website);
+    var urlHit = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=38fb3300";
+    axios.get(urlHit).then(
+        function(response)
+        {
+            if (error.response.statusCode === 200) {
+                console.log("Title of the movie: " + JSON.parse(body).Title);
+                console.log("Year the movie came out: " + JSON.parse(body).Year);
+                console.log("IMDB Rating of the movie: " + JSON.parse(body).Rating);
+                console.log("Country produced: " + JSON.parse(body).Country);
+                console.log("Language of the movie: " + JSON.parse(body).Language);
+                console.log("Plot of the movie: " + JSON.parse(body).Plot);
+                console.log("Actors in the movie: " + JSON.parse(body).Actors);
+    
+                for(var i = 0; i < JSON.parse(body).Ratings.length; i++) {
+                    if(JSON.parse(body).Ratings[i].Source === "Rotten Tomatoes") {
+                        console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[i].value);
+                        if(JSON.parse(body).Ratings[i].Website !== undefined) {
+                            console.log("Rotten Tomatoes URL: " + JSON.parse(body).Ratings[i].Website);
+                        }
                     }
                 }
             }
-        }
-    })
+        } )
 } 
 
 //if state similar to spotify default song = movie, 
