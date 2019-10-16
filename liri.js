b42ee0ff0b05d44ab5acf3888b60f260e04095fd
Maliah Lacy <mlacy0915@gmail.com>
2019-10-15 19:13:40 -0500

    movie this rotten tomatoes added

--- liri.js
+++ liri.js
@@ -1,4 +1,3 @@
-console.log (process.argv)
 
 require("dotenv").config();
 
@@ -9,6 +8,24 @@ var moment = require('moment');
 var bandsintown = require('bandsintown')(APP_ID);
 var keys = require("./keys");
 var spotify = new Spotify(keys.spotify);
+var require = require("request");
+
+function UserImputs (userOption, inputParameter) {
+    switch (userOption) {
+        case 'concert-this':
+            showConcertInfo(inputParameter);
+            break;
+        case 'spotify-this-song':
+            showSongInfo(inputParameter);
+            break;
+        case 'movie-this':
+            showMovieInfo(inputParameter);
+            break;
+        case 'do-what-it-says':
+            showSomeInfo();
+            break;
+    }
+}
 
 var artistName = function(artist) {
     return artist.name
@@ -41,28 +58,74 @@ var searchSpotify = function(songName) {
     )
 }
 //band in town fuction 
-var bandsearch = function(bandsTown) {
-    if (bandsTown === undefined) { 
-        console.log("No Upcoming Shows")
-    }
-    bandsintown.search ( 
-        {
-        type: "info",
-        query:"events",
-        }
-        function(err,data) {
-            if (err) {
-                console.log("Error: " + err);
-                return;
-    )
-}
+
 // call to api .axios call. for loop of data. moment to fomat data
 
 
 //movie search function,
+var movieThis = funtion(inputParameter) {
+    if (inputParameter === undefined) {
+        inputParameter = "Mr. Nobody"
+        console.log("If you haven't watched 'Mr. Nobody,' then you should: <http://www.imdb.com/title/tt0485947/>");
+    }
+    var queryUrl = "http://www.omdbapi.com/?t=" + inputParameter + "&y=&plot=short&apikey=codingbootcamp";
+    request(queryUrl, funtion(err.response,body) {
+        if (error.response.statusCode === 200) {
+            console.log("Title of the movie: " + JSON.parse(body).Title);
+            console.log("Year the movie came out: " + JSON.parse(body).Year);
+            console.log("IMDB Rating of the movie: " + JSON.parse(body).Rating);
+            console.log("Country produced: " + JSON.parse(body).Country);
+            console.log("Language of the movie: " + JSON.parse(body).Language);
+            console.log("Plot of the movie: " + JSON.parse(body).Plot);
+            console.log("Actors in the movie: " + JSON.parse(body).Actors);
+
+            for(var i = 0; i < JSON.parse(body).Ratings.length; i++) {
+                if(JSON.parse(body).Ratings[i].Source === "Rotten Tomatoes") {
+                    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[i].value);
+                    if(JSON.parse(body).Ratings[i].Website !== undefined) {
+                        console.log("Rotten Tomatoes URL: " + JSON.parse(body).Ratings[i].Website);
+                    }
+                }
+            }
+        }
+    })
+} 
+
 //if state similar to spotify default song = movie, 
 //axios call to omdb, console.log, 
+var movieName=process.argv[2]
+
+var queryUrl = "http://www.omdbapi.com/?apikey=[codingbootcamp]&" + movieName + "&y=&plot=short&apikey=codingbootcamp"; 
+console.log(queryUrl);
+
+axios.get(queryUrl).then(
+    function(response) {
+        console.log("Release Year" + response.data.Year);
+    }
+)
+.catch(function(err) {
+    if (err.response) {
+
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+        console.log(err.request);
+    } else {
+        console.log("Error", error.message);
+    }
+    console.log(err.config);
+})
 // do what it says function, set up err, switch case,
+
+
+
+
+
+
 
 // console.log(process.argv)
 
